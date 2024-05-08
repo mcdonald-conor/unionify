6bb701d6ab9ac69da954ee9d80b11e7945229a32
Sofia Singam <sofiasingam@gmail.com>
2024-05-08 14:43:53 +0100

    new page form completed

--- app/models/celebrant.rb
+++ app/models/celebrant.rb
@@ -2,7 +2,7 @@ class Celebrant < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :username, prescene: true, uniqueness: true
-  validates :location, prescence: true
-  validates :rate, prescence: true
+  #validates :username, prescence: true, uniqueness: true
+  #validates :location, prescence: true
+  #validates :rate, prescence: true
 end
