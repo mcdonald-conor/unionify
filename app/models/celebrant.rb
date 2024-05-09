0c849d3e95eae65674cf57403f364dcaf01d44a4
Conor McDonald <cm@mcdonaldconor.com>
2024-05-09 15:12:46 +0100

    removed through => bookings so that the server works for now

--- app/models/celebrant.rb
+++ app/models/celebrant.rb
@@ -1,7 +1,5 @@
 class Celebrant < ApplicationRecord
-  belongs_to :user, through => :bookings
-  has_many :bookings
-
+  belongs_to :user
   #validates :username, prescence: true, uniqueness: true
   #validates :location, prescence: true
   #validates :rate, prescence: true
